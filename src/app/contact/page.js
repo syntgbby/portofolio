32ae4cbd654ef7b7420aa6f47f3b8e3bef423403
syntgbby <beezeelvly@gmail.com>
2024-09-09 20:13:08 +0700

    first commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,19 @@
+export default function Contact(){
+    return (
+        <> 
+        <h2 className="text-center text-3xl w-full">Get In Touch</h2>
+        <p className="text-center w-full">lorem ipsum loremlorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum </p>
+        <div className="grid grid-rows-2 grid-flow-col gap-4">
+            <div className="col-start-2 col-span-4 border">kiri</div>
+            <div className="col-end-2 border">kanan
+            </div>
+        </div>
+
+        <div>
+            
+        </div>
+
+        </>
+    );
+}
---
+++ src/app/work/page.js
@@ -0,0 +1,5 @@
+export default function work(){
+    return (
+        <>work</>
+    );
+}
