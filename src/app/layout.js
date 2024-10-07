33c08a16504cb583ecd2883b5e7211b95d195ea3
syntgbby <beezeelvly@gmail.com>
2024-10-07 21:16:55 +0700

    new new

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,6 @@
 "use client"; // This tells Next.js that this is a Client Component
 
 import { Inter } from "next/font/google";
-import { useState } from "react";
-import Navbar from "@/components/nav";
-import Sidebar from "@/components/sidebar";
-import { ThemeProvider } from "@/components/theme-provider";
-import { ViewTransitions } from 'next-view-transitions'
 
 const inter = Inter({ subsets: ["latin"] });
 
